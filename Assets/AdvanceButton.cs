13bd7642b048ead8e65b4c2ec338002f50f3df8e
SonicHedghog <kylehenry7000@gmail.com>
2022-02-17 17:25:14 -0600

    Added Ability to Flow between Menus (No back button yet)

---
+++ Assets/AdvanceButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AdvanceButton : MonoBehaviour
+{
+    public GameObject scene;
+    public GameObject thisScene;
+    public bool isScene = false;
+    public bool isBack = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Advance()
+    {
+        if(isScene)
+        {
+            SceneManager.LoadScene(this.gameObject.name);
+        }
+        else
+        {
+            scene.SetActive(true);
+            thisScene.SetActive(false);
+        }
+    }
+}
