4ef73d705dec13cb0a98f88a71d68c252cf56242
SonicHedghog <kylehenry7000@gmail.com>
2022-02-19 15:45:46 -0600

    Added DefaultQuestionGenerator File

---
+++ Assets/Scripts/Study Time API/DefaultQuestionGenerator.cs
@@ -0,0 +1,56 @@
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using StudyTimeAPI;
+
+
+public class DefaultQuestionGenerator : QuestionGenerator
+{
+    private List<string> Questions;
+    private bool RandomSelect;
+    private int index = 0;
+
+    public DefaultQuestionGenerator()
+    {
+        RandomSelect = "true" == FileManager.configs["random"].ToLower();
+        
+        if(Application.platform == RuntimePlatform.Android)
+        {
+            var www = UnityEngine.Networking.UnityWebRequest.Get(Path.Combine(FileManager.path, "default_questions.in"));
+            www.SendWebRequest();
+            while (!www.isDone)
+            {
+            }
+            Questions = www.downloadHandler.text.Split('\n').ToList();
+            Debug.Log(www.downloadHandler.text);
+        }
+        else
+        {
+            Questions = File.ReadAllLines(Path.Combine(FileManager.path, "default_questions.in")).ToList();
+        }
+    }
+
+    public override string GetQuestion()
+    {
+        if(!RandomSelect && index < Questions.Count) 
+            return Questions[index++];
+
+        index = Random.Range(0, Questions.Count);
+
+        string question = Questions[index];
+        Questions.RemoveAt(index);
+        return question;
+    }
+
+    public override List<string> GetQuestionList()
+    {
+        return Questions;
+    }
+
+    public override bool IsComplete()
+    {
+        return index < Questions.Count;
+    }
+    
+}
