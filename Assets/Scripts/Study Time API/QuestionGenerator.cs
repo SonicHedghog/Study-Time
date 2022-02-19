24e1c59a992a06bffdecae819fab60089cbe844d
SonicHedghog <kylehenry7000@gmail.com>
2022-02-19 08:51:37 -0600

    Added QuestionGenerator to Study Time API

---
+++ Assets/Scripts/Study Time API/QuestionGenerator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class QuestionGenerator
+{
+    public QuestionGenerator() { }
+    public abstract string GetQuestion();
+}
