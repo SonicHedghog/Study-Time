da322164c6fc6af86e1398fb3ded2ea4bc8a372d
SonicHedghog <kylehenry7000@gmail.com>
2022-02-20 01:13:20 -0600

    Fixed some namespace issues

--- Assets/Scripts/Study Time API/DefaultQuestionGenerator.cs
+++ Assets/Scripts/Study Time API/DefaultQuestionGenerator.cs
@@ -31,7 +31,7 @@ public class DefaultQuestionGenerator : QuestionGenerator
         }
     }
 
-    public override string GetQuestion(Object o = null)
+    public override string GetQuestion(System.Object o = null)
     {
         if(!RandomSelect && index < Questions.Count) 
             return Questions[index++];
